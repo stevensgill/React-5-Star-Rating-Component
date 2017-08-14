4aea3cb6c82c5cad528e5376691ebb9a7b062746
Steve Gill <steven.s.gill@gmail.com>
2017-08-14 12:28:04 -0400

    Update RatingComponent.jsx

--- RatingComponent.jsx
+++ RatingComponent.jsx
@@ -1,64 +1,71 @@
-function RatingFeedbackPopover ({
+function RatingStar ({
+  selected = false,
   rating,
-  feedback,
   handleStarClick,
-  handleClearClick,
-  handleFeedbackChange,
   handleStarHoverOver,
-  handleStarHoverOut,
-  submitRating,
-  hasSaved
+  handleStarHoverOut
 }) {
-  const { ButtonToolbar, Button } = ReactBootstrap;
-  const {
-    label,
-    feedback_placeholder: feedbackPlaceholder,
-    buttons: {
-      add_comment: addCommentLabel,
-      clear_rating: clearRatingLabel,
-      update_comment: updateCommentLabel
-    }
-  } = I18n.t('investigations.scores.popover');
+  let starClasses = 'RatingRating__star fa fa-star';
+  if (selected) {
+    starClasses += ' selected';
+  }
   return (
-    <div className="RatingFeedbackPopover">
-      <RatingRating
-        rating={rating}
+    <i className={starClasses}
+       onClick={() => handleStarClick(rating)}
+       onMouseOver={() => handleStarHoverOver(rating)}
+       onMouseOut={() => handleStarHoverOut()}
+    />
+  );
+};
+
+RatingStar.propTypes = {
+  rating: React.PropTypes.number,
+  handleStarClick: React.PropTypes.func.isRequired,
+  handleStarHoverOver: React.PropTypes.func.isRequired,
+  handleStarHoverOut: React.PropTypes.func.isRequired
+};
+
+RatingStar.defaultProps = {
+  rating: 0,
+};
+
+
+function RatingComponent ({
+  rating = 0,
+  handleStarClick,
+  handleStarHoverOver,
+  handleStarHoverOut
+}) {
+  const { header_label: headerLabel } = I18n.t('investigations.scores');
+  let stars = [];
+  for(let i = 0 ; i < 5; i++){
+    const selected = (i < rating);
+    stars.push(
+      <RatingStar
+        key={i}
+        selected={selected}
+        rating={i + 1}
         handleStarClick={handleStarClick}
         handleStarHoverOver={handleStarHoverOver}
         handleStarHoverOut={handleStarHoverOut}
       />
-      <textarea
-        className="RatingFeedbackPopover__feedback"
-        name="feedback"
-        defaultValue={feedback}
-        placeholder={feedbackPlaceholder}
-        onChange={(event) => handleFeedbackChange(event.target.value)}
-      ></textarea>
-      <ButtonToolbar>
-        <Button
-          disabled={_.isNull(rating)}
-          className="RatingFeedbackPopover__addComment btn btn-default"
-          onClick={submitRating}>{addCommentLabel}</Button>
-        <a className="RatingFeedbackPopover__clearRating"
-           href="#"
-           onClick={handleClearClick}>{clearRatingLabel}</a>
-      </ButtonToolbar>
+    );
+  }
+
+  return (
+    <div className="RatingComponent">
+      <label className="RatingComponent__label">{headerLabel}</label>
+      {stars}
     </div>
   );
 };
 
-RatingFeedbackPopover.propTypes = {
+RatingComponent.propTypes = {
   rating: React.PropTypes.number,
-  feedback: React.PropTypes.string,
   handleStarClick: React.PropTypes.func.isRequired,
-  handleClearClick: React.PropTypes.func.isRequired,
-  handleFeedbackChange: React.PropTypes.func.isRequired,
   handleStarHoverOver: React.PropTypes.func.isRequired,
-  handleStarHoverOut: React.PropTypes.func.isRequired,
-  submitRating: React.PropTypes.func.isRequired,
-  hasSaved: React.PropTypes.bool.isRequired
+  handleStarHoverOut: React.PropTypes.func.isRequired
 };
 
-RatingFeedbackPopover.defaultProps = {
-  feedback: null,
+RatingComponent.defaultProps = {
 };
